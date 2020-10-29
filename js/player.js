6462dbd7ef28b83a4435b8f0e00a7306ccc17691
nostam <matsonho@gmail.com>
2020-10-29 16:42:23 +0100

    splitting js files

--- js/player.js
+++ js/player.js
@@ -1,14 +1,4 @@
 window.onload = function () {
-  let btn = document.querySelectorAll(".menu ul");
-  let list = document.querySelector(".playlists");
-  btn[1].firstElementChild.addEventListener("click", function () {
-    let newItem = document.createElement("p");
-    newItem.innerText = "New Playlist #";
-    newItem.innerText += list.childElementCount++;
-    console.log(newItem);
-    list.insertBefore(newItem, list.firstElementChild);
-  });
-
   let heartOutline = document.querySelector(".loved-track .far.fa-heart");
   let heartFilled = document.querySelector(".loved-track .fas.fa-heart");
   heartFilled.addEventListener("click", function () {
@@ -27,6 +17,7 @@ window.onload = function () {
   let volMax = document.querySelector(".player-volume .fas.fa-volume-up");
   muted.addEventListener("click", function () {
     if (!muted.classList.contains("active")) {
+      console.log("going mute");
       muted.classList.toggle("active");
       muted.style.opacity = 1;
       volMax.style.opacity = 0;
